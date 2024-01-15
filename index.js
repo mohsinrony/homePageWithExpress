167cd211453dfc0fe24530681b99309c24b40daa
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-15 12:12:42 +0200

    create another page

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// const app = require('express')();
+
+const port = 3000;
+const host = 'localhost';
+
+const homePath = path.join(__dirname, 'home.html');
+const pageBpath = path.join(__dirname, 'pageB.html');
+
+app.get('/', (req, res) => {res.sendFile(homePath)});
+app.get('/pageb', (req,res)=> {res.sendFile(pageBpath)});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+} );
